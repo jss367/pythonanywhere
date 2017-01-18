2843580d35c61fc3e94237c15f255464ad199d68
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-18 17:49:45 +0930

    flask again :(

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,49 @@
-from flask import Flask
 
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, redirect, render_template, request, url_for, jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+from text_analysis import analyze_text
+#maybe this will help?
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="jss367",
+    password="mysqldbpw0o0o",
+    hostname="jss367.mysql.pythonanywhere-services.com",
+    databasename="jss367$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 
-def hello_world():
-    return 'Hello from Flask!'
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments = Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/about')
+def about_route():
+    return render_template('about.html')
+
+
+@app.route('/analyze-text', methods=['POST'])
+def analyze():
+    html = request.form.get('html', '')
+    text, tokens, metrics = analyze_text(html)
+    return jsonify({'text': text, 'tokens': tokens, 'metrics': metrics})
