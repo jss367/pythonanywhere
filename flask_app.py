60837b5501341933b22911710a08ee941086e711
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-24 17:35:46 +0930

    threw some logic into index

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,12 @@
 from flask import Flask, redirect, render_template, request, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
+import re
+import nltk
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -23,8 +29,6 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-comments = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
@@ -38,4 +42,45 @@ def index():
 
 @app.route('/index', methods=['GET', 'POST'])
 def new_index():
-    return render_template('index.html')
+	errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the person has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+            return render_template('index.html', errors=errors)
+        if r:
+            # text processing
+            raw = Comment(content=request.form["contents"])
+            nltk.data.path.append('./nltk_data/')  # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+    return render_template('index.html', errors=errors, results=results)
