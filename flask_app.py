d32a2f189d5a0ac495f9db3b02c604817407be7d
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-24 11:56:56 +0930

    Revert "added html template"
    This reverts commit 7931b8084a6e1dcc0d003d8a9c1f736faae8d026.

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
-app.config["DEBUG"] = True
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("main_page.html")
+@app.route('/')
+def hello_world():
+    return 'Hello from the other side!'
 
