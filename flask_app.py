eae78ccdbedb639811e44e962e413583b7a18767
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-24 14:09:27 +0930

    fixed username

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="sappho$sapphodb",
+    username="sappho",
     password="mysqldbpw0o0o",
     hostname="sappho.mysql.pythonanywhere-services.com",
     databasename="sappho$sapphodb",
@@ -17,14 +17,14 @@ db = SQLAlchemy(app)
 
 class Comment(db.Model):
 
-    __tablename__ = "commentdb"
+    __tablename__ = "sapphodb"
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
 class Feature(db.Model):
 
-    __tablename__ = "featuredb"
+    __tablename__ = "sapphodb"
 
     id = db.Column(db.Integer, primary_key=True)
     feature_content = db.Column(db.String(4096))
