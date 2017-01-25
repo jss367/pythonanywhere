604f2447b4ecc408212e5c2155d90a466dedbdb7
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-25 15:52:43 +0930

    fixes typo

--- text_analysis.py
+++ text_analysis.py
@@ -1,5 +1,5 @@
 def analyze_text(text):
-	nonPunct = re.compile('.*[A-Za-z].*')
+    nonPunct = re.compile('.*[A-Za-z].*')
     raw_words = [w for w in text if nonPunct.match(w)]
     raw_word_count = Counter(raw_words)
     results = sorted(
