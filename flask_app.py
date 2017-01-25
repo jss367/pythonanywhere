fb28f678258d9730e483ee72b09995bf06931c03
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-25 16:17:59 +0930

    undoing text to comment

--- flask_app.py
+++ flask_app.py
@@ -38,9 +38,10 @@ def index():
     elif request.method == "POST":
         errors = []
         results = {}
-        text = Comment(content=request.form["contents"])
-        db.session.add(text)
+        comment = Comment(content=request.form["contents"])
+        db.session.add(comment)
         db.session.commit()
+        text = comment
         results = analyze_text(text)
         return render_template('index.html', errors=errors, results=results)
     else:
