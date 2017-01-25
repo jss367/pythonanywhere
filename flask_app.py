c26db65e74cfa68ed61a1a3ed058c746dff78028
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-25 10:04:22 +0930

    trying post method

--- flask_app.py
+++ flask_app.py
@@ -42,34 +42,38 @@ def index():
 
 @app.route('/index', methods=['GET', 'POST'])
 def new_index():
-    errors = []
-    results = {}
     if request.method == "POST":
-        raw = Comment(content=request.form["contents"])
-        nltk.data.path.append('./nltk_data/')  # set the path
-        tokens = nltk.word_tokenize(raw)
-        text = nltk.Text(tokens)
-        # remove punctuation, count raw words
-        nonPunct = re.compile('.*[A-Za-z].*')
-        raw_words = [w for w in text if nonPunct.match(w)]
-        raw_word_count = Counter(raw_words)
-        # stop words
-        no_stop_words = [w for w in raw_words if w.lower() not in stops]
-        no_stop_words_count = Counter(no_stop_words)
-        # save the results
-        results = sorted(
-            no_stop_words_count.items(),
-            key=operator.itemgetter(1),
-            reverse=True
-            )
-        try:
-            result = Result(
-                url=url,
-                result_all=raw_word_count,
-                result_no_stop_words=no_stop_words_count
-                )
-            db.session.add(result)
-            db.session.commit()
-        except:
-            errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
+        return render_template('post.html')
+    if request.method == "GET":
+        return render_template('index.html',)
+    # errors = []
+    # results = {}
+    # if request.method == "POST":
+    #     raw = Comment(content=request.form["contents"])
+    #     nltk.data.path.append('./nltk_data/')  # set the path
+    #     tokens = nltk.word_tokenize(raw)
+    #     text = nltk.Text(tokens)
+    #     # remove punctuation, count raw words
+    #     nonPunct = re.compile('.*[A-Za-z].*')
+    #     raw_words = [w for w in text if nonPunct.match(w)]
+    #     raw_word_count = Counter(raw_words)
+    #     # stop words
+    #     no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    #     no_stop_words_count = Counter(no_stop_words)
+    #     # save the results
+    #     results = sorted(
+    #         no_stop_words_count.items(),
+    #         key=operator.itemgetter(1),
+    #         reverse=True
+    #         )
+    #     try:
+    #         result = Result(
+    #             url=url,
+    #             result_all=raw_word_count,
+    #             result_no_stop_words=no_stop_words_count
+    #             )
+    #         db.session.add(result)
+    #         db.session.commit()
+    #     except:
+    #         errors.append("Unable to add item to database.")
+    # return render_template('index.html', errors=errors, results=results)
