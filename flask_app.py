a5d41e29aa4f6b1e07163ae60a020ec8e1122170
Julius Simonelli <julius.simonelli@gmail.com>
2017-01-26 17:13:28 +0930

    Revert "ok, i think i found it"
    This reverts commit 11cb9ad08c50afdc8eb8a20f5bdacc85328f1099.

--- flask_app.py
+++ flask_app.py
@@ -37,16 +37,10 @@ def index():
     elif request.method == "POST":
         errors = []
         results = {}
-        comment = Comment(content=request.form["contents"]) #this and the below requests could be much better written
+        comment = Comment(content=request.form["contents"])
         db.session.add(comment)
         db.session.commit()
-        try:
-            text = request.form['contents']
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
-            )
-        results = analyze_text(text)
+        results = analyze_text(comment)
         return results
         #return render_template("main_page.html", comments=Comment.query.all(), errors=errors, results=results)
         #return redirect(url_for('index'))
