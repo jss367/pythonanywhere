e7316458891b80eaf39d3544ee75c993307071f6
Julius Simonelli <julius.simonelli@gmail.com>
2017-02-02 21:15:53 +0930

    syncing

--- text_analysis.py
+++ text_analysis.py
@@ -1,15 +1,28 @@
 import re
 import nltk
-from stop_words import stops
 from collections import Counter
 from bs4 import BeautifulSoup
 
-def analyze_text(text):
-    nonPunct = re.compile('.*[A-Za-z].*')
-    raw_words = [w for w in text if nonPunct.match(w)]
-    raw_word_count = Counter(raw_words)
+def analyze_text2(text):
+    tokens = clean_text(text)
+    num_words = word_count(tokens)
+    ave_word = ave_word_size(tokens)
+    
     results = sorted(
-        raw_word_count.items(),
+        num_words.items(),
         reverse=True
         )
     return results
+
+def clean_text(raw_text):
+    tokens = nltk.word_tokenize(raw_text)
+    return tokens
+
+def word_count(tokens):
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in tokens if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+    return raw_word_count
+
+def ave_word_size(tokens):
+    return float(sum(map(len, tokens))) / len(tokens)
