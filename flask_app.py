63fe72c7c93c5124bcd8b8403c04a0715a3907ac
Julius Simonelli <julius.simonelli@gmail.com>
2017-02-02 23:00:37 +0930

    removed extra text

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,13 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template#, request, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from text_analysis import analyze_text
 from text_analysis2 import analyze_text2
 
-import re
-import nltk
-from stop_words import stops
-from collections import Counter
-from bs4 import BeautifulSoup
+#import re
+#import nltk
+#from stop_words import stops
+#from collections import Counter
+#from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -47,9 +47,6 @@ def index():
             )
         results = analyze_text(text)
     return render_template("main_page.html", errors=errors, results=results)
-        #return render_template("main_page.html", comments=Comment.query.all(), errors=errors, results=results)
-        #return redirect(url_for('index'))
-
 
 @app.route('/index', methods=['GET', 'POST'])
 def new_index():
@@ -63,25 +60,6 @@ def new_index():
                 "Unable to get URL. Please make sure it's valid and try again."
             )
         results = analyze_text2(text)
-        #raw = Comment(content=request.form["contents"])
-        #nltk.data.path.append('./nltk_data/')  # set the path
-        #tokens = nltk.word_tokenize(raw)
-        #text = nltk.Text(tokens)
-        # save the results
-        #results = sorted(
-        #    raw_word_count.items(),
-        #    reverse=True
-        #   )
-    #    try:
-    #        result = Result(
-    #            url=url,
-    #            result_all=raw_word_count,
-    #            result_no_stop_words=no_stop_words_count
-    #            )
-    #        db.session.add(result)
-    #       db.session.commit()
-    #   except:
-    #        errors.append("Unable to add item to database.")
     return render_template('index.html', errors=errors, results=results)
 
 
